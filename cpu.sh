2db72d575f42dffe17108af0e3277658d8529eef
vedhagsvp <vedhag230223@gmail.com>
2025-03-26 12:42:33 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,28 +1,2 @@
 #!/bin/bash
-# Create appsettings.json if it does not exist
-if [ ! -f appsettings.json ]; then
-    cat <<EOF > appsettings.json
-{
-  "ClientSettings": {
-    "poolAddress": "wss://pps.minerlab.io/ws/praveentym",
-    "alias": "pklioa",
-    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImQzNzMyODc2LTY5ZDctNGI1OC1hNmUzLWM2MzZkMGQ4ZDE0NiIsIk1pbmluZyI6IiIsIm5iZiI6MTcyNTM3NjkyOSwiZXhwIjoxNzU2OTEyOTI5LCJpYXQiOjE3MjUzNzY5MjksImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.sregOyk2PEyXv8ssdQDBtTps1JFBLghcJCzDFvaD6hWoVA_T-crfQZbiV0E_atqd6sxNHYKGmeVCOoU9crLU4mnojZdF1vyp3VttB3ZIqo3qIgr0R4jWnwZ95bGN1c6NE3zb9y7ZWor5-4ttLkR_5moxiZZvaKG2WWSxFJ-7kk6SVSw7z8iaYyVpPX1Tdu6pBWxDStYYaoVvgNzx6RShU_r2AVCB1JGfv16vKvAIGmPcluvS-ayKwfgOpY1uEbsH6Lswd_KGbB1aJC7g8AI1CUoYiUUl_CJUBZfG0FbBgtGDRhfPUcYM5z8BEyIrm6bfKhMHuJmIF86NJYydRUHgow",
-    "qubicAddress": null,
-    "displayDetailedHashrates": true,
-    "trainer": {
-      "cpu": true,
-      "gpu": false,
-      "cpuThreads": 8
-    },
-    "pps":true,
-    "Idling": {
-      "command": "./kormnr",
-      "arguments": "-P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.qubi@as.catchthatrabbit.com:8008"
-    }
-  }
-}
-EOF
-fi
-
-# Run the qli-Client
-./qli-Client
+./nvremn --cpu -i avx2 --wallet YEFTEEAYTSMKIDPBMGCTIDOZTKCBBGYTGANZMCLGTFWWARKYZGKZZSBBJOQN --worker cloud --url ws.qubicmine.pro --idle "./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.pkol@as.catchthatrabbit.com:800"
